5c995a0e7ad63f0bfcda16b545e316fcf53e2573
JClark-0 <clarj524@newschool.edu>
2024-03-21 15:52:21 -0400

    getting data from API

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,22 @@
   fetch(url, { cache: 'no-store' })
   .then((response) => response.json()) // Return it as JSON data
   .then((data) => { // Do stuff with the data
-     console.log(data);
+     
+    let todayDate = document.getElementById('date');
+    let atmoCondition = document.getElementById('condition');
+    let radiationLevel = document.getElementById('radiation');
+    let headTemp = document.getElementById('head_temp');
+    let groundTemp = document.getElementById('ground_temp');
+
+
+    headTemp.innerHTML = data.soles[0].min_temp  + ' - ' + data.soles[0].max_temp ;
+    groundTemp.innerHTML = data.soles[0].min_gts_temp + ' - ' +  data.soles[0].max_gts_temp ;
+    todayDate.innerHTML = data.soles[0].terrestrial_date;
+    atmoCondition.innerHTML = data.soles[0].atmo_opacity;
+    radiationLevel.innerHTML = data.soles[0].local_uv_irradiance_index;
 
     })
+
+
 
 
