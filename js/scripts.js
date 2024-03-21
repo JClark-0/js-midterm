77f75c71f8b7d67cf0f506d10263bba2ea4a1ef4
JClark-0 <clarj524@newschool.edu>
2024-03-21 15:20:32 -0400

    Mars API

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,11 @@
-let button = document.getElementById('GetUsers');
 
-button.addEventListener("click", getUserData);
-
-
-
-function getUserData() {
-  var url = "https://reqres.in/api/users";
-  var xhr = new XMLHttpRequest();
+  var url = "https://mars.nasa.gov/rss/api/?feed=weather&category=msl&feedtype=json";
  
-  xhr.onload = function() {
-        	if (xhr.status === 200) {
-    
-                const authors = JSON.parse(xhr.responseText);
-                //format users
+  fetch(url, { cache: 'no-store' })
+  .then((response) => response.json()) // Return it as JSON data
+  .then((data) => { // Do stuff with the data
+     console.log(data);
 
-                const ul = document.createElement('ul');
-                for (user of authors.data) {
+    })
 
-                    let li = document.createElement('li'),
-                        img = document.createElement('img'),
-                        span = document.createElement('span');
 
-                    img.src = user.avatar;
-                    span.innerHTML = user.first_name + " " + user.last_name;
-
-                    li.appendChild(img);
-                    li.appendChild(span);
-                    ul.appendChild(li);
-
-                }
-
-            document.body.appendChild(ul);
-
-        	} else {
-      			document.getElementById("Output").innerHTML = "There was an error";
-        	}
-
-  }
-  xhr.open("GET", url, true);
-  xhr.send();
-}
